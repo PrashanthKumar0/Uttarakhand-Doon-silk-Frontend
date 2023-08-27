4f2fc1aebee61bf390d8d54065f8284d2ef27d43
Kushal Poddar <kpoddar160200@gmail.com>
2023-08-28 01:58:44 +0530

    Baseurl to https

--- src/Url.jsx
+++ src/Url.jsx
@@ -1,5 +1,5 @@
 // export const baseUrl= 'http://52.66.55.10'
 // export const baseImgUrl='http://52.66.55.10/public/image/'
 
-export const baseUrl= "http://api.doonsilk.com"
-export const baseImgUrl='http://api.doonsilk.com/public/images'
+export const baseUrl= "https://api.doonsilk.com"
+export const baseImgUrl='https://api.doonsilk.com/public/images'
