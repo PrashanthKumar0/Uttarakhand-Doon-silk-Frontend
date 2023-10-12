dec1b21cf3e28d418eb4765b2121defc6fb31a1a
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2023-10-12 18:53:29 +0530

    Fixes : Tender Page
    Fixes
    - Cannot access uploaded Pdf files in tender(GET API error)
    - Links out of bounds in tender

--- src/app/tender/TextTender.jsx
+++ src/app/tender/TextTender.jsx
@@ -1,10 +1,19 @@
+import Link from 'next/link';
 import React from 'react'
 
 export default function TextTender({txt}) {
+  const url = (new RegExp("(http[s]?:\/\/.{0,})")).exec(txt)[0];
+  txt = txt.split(url);
   return (
     <div className='py-2'>
     <div style={{backgroundColor:'white', borderRadius:'10px'}} className='p-1 mx-1 text-neutral-800 '>
-   <div className='px-2 downloadbtn'>{txt}</div>
+   <div className='px-2 downloadbtn'>
+    {txt[0]}
+      { url?
+        <Link href={url}>url</Link>:''
+      }
+    {txt[1]}
+   </div>
    </div>
    </div>
   )
