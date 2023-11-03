692ffa7d6f90b567a627e5ae805b2403f2fe7711
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2023-11-03 23:21:07 +0530

    Updated Map

--- src/app/Crousal/page.tsx
+++ src/app/Crousal/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import axios from 'axios';
@@ -17,25 +17,31 @@ const Crousal: React.FC = () => {
     image2: '',
     image3: '',
   });
-  useEffect(()=>{
-    axios.get(`${baseUrl}/getBanner`)
-    .then((response)=>{
-     console.log('responseBanner' ,response)
-      setImages({
-        image1: response.data.data.image1 , 
-        image2: response.data.data.image2 ,
-        image3: response.data.data.image3 ,
-       
+  useEffect(() => {
+    setImages({
+      image1: '/banner.jpg',
+      image2: '/banner.jpg',
+      image3: '/banner.jpg',
+    })
 
-      })
-      console.log('images',images)
-      //setImages(response.data.data[0]),
-     // console.log('getbannr',images)
-    }
-     )
-    .catch((error)=>{console.log(error)})
-  
-  },[])
+    // axios.get(`${baseUrl}/getBanner`)
+    // .then((response)=>{
+    //  console.log('responseBanner' ,response)
+    //   setImages({
+    //     image1: response.data.data.image1 , 
+    //     image2: response.data.data.image2 ,
+    //     image3: response.data.data.image3 ,
+
+
+    //   })
+    //   console.log('images',images)
+    //setImages(response.data.data[0]),
+    // console.log('getbannr',images)
+    // }
+    //  )
+    // .catch((error)=>{console.log(error)})
+
+  }, [])
   return (
     <div className="w-full h-full " >
       <Swiper
@@ -48,20 +54,23 @@ const Crousal: React.FC = () => {
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
       >
-       
-          <SwiperSlide>
-            <img src={`${baseImgUrl}/${images.image1}`} alt={`${baseImgUrl}/${images.image1} `} className="w-full h-full object-cover" />
-           
-          </SwiperSlide>
-          <SwiperSlide>
-          
-       <img src={`${baseImgUrl}/${images.image2}`} alt={`Banner `} className="w-full h-full object-cover" />
-          
-          </SwiperSlide>
-          <SwiperSlide>
-           
-            <img src={`${baseImgUrl}/${images.image3}`} alt={`Banner `} className="w-full h-full object-cover" /> 
-          </SwiperSlide>      
+
+        <SwiperSlide>
+          {/* <img src={`${baseImgUrl}/${images.image1}`} alt={`${baseImgUrl}/${images.image1} `} className="w-full h-full object-cover" /> */}
+          <img src={`${images.image1}`} alt={`${images.image1}`} className="w-full h-full object-cover" />
+
+        </SwiperSlide>
+        <SwiperSlide>
+
+          {/* <img src={`${baseImgUrl}/${images.image2}`} alt={`Banner `} className="w-full h-full object-cover" /> */}
+          <img src={`${images.image2}`} alt={`${images.image1}`} className="w-full h-full object-cover" />
+
+        </SwiperSlide>
+        <SwiperSlide>
+
+          {/* <img src={`${baseImgUrl}/${images.image3}`} alt={`Banner `} className="w-full h-full object-cover" /> */}
+          <img src={`${images.image3}`} alt={`${images.image1}`} className="w-full h-full object-cover" />
+        </SwiperSlide>
       </Swiper>
     </div>
   );
--- src/app/about/PageAbout.jsx
+++ src/app/about/PageAbout.jsx
@@ -64,11 +64,10 @@ const PageAbout = ({ }) => {
 
         <SectionStatistic />
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <iframe loading="lazy" src="https://maps.google.com/maps?q=Doon%20Sild%20Tyagi%20Market&amp;t=m&amp;z=10&amp;output=embed&amp;iwloc=near"
-            title="silk park bhawan premanagr dehradun" aria-label="silk park bhawan premanagr dehradun" className="w-100" style={{ width: '80vw', height: '50vh' }}></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3443.527405645236!2d77.96277967556495!3d30.33595767477822!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39092b5ca23b57c3%3A0x22913355728bbfce!2sDOON%20SILK!5e0!3m2!1sen!2sin!4v1699033595448!5m2!1sen!2sin" width="600" height="450" style={{border:'0px'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
-        <SectionPromo3 />
+        {/* <SectionPromo3 /> */}
       </div>
     </div>
   );
