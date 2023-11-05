2552908cc2d423691b07c4761b6306c359a07f1f
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2023-11-05 16:08:31 +0530

    Fix Static Content Url

--- src/app/Crousal/page.tsx
+++ src/app/Crousal/page.tsx
@@ -18,28 +18,28 @@ const Crousal: React.FC = () => {
     image3: '',
   });
   useEffect(() => {
-    setImages({
-      image1: '/banner.jpg',
-      image2: '/banner.jpg',
-      image3: '/banner.jpg',
-    })
-
-    // axios.get(`${baseUrl}/getBanner`)
-    // .then((response)=>{
-    //  console.log('responseBanner' ,response)
     //   setImages({
-    //     image1: response.data.data.image1 , 
-    //     image2: response.data.data.image2 ,
-    //     image3: response.data.data.image3 ,
-
-
+    //     image1: '/banner.jpg',
+    //     image2: '/banner.jpg',
+    //     image3: '/banner.jpg',
     //   })
-    //   console.log('images',images)
-    //setImages(response.data.data[0]),
-    // console.log('getbannr',images)
-    // }
-    //  )
-    // .catch((error)=>{console.log(error)})
+
+    axios.get(`${baseUrl}/getBanner`)
+      .then((response) => {
+        console.log('responseBanner', response)
+        setImages({
+          image1: response.data.data.image1,
+          image2: response.data.data.image2,
+          image3: response.data.data.image3,
+
+
+        })
+        console.log('images', images)
+        setImages(response.data.data[0]),
+          console.log('getbannr', images)
+      }
+      )
+      .catch((error) => { console.log(error) })
 
   }, [])
   return (
@@ -56,20 +56,20 @@ const Crousal: React.FC = () => {
       >
 
         <SwiperSlide>
-          {/* <img src={`${baseImgUrl}/${images.image1}`} alt={`${baseImgUrl}/${images.image1} `} className="w-full h-full object-cover" /> */}
-          <img src={`${images.image1}`} alt={`${images.image1}`} className="w-full h-full object-cover" />
+          <img src={`${baseImgUrl}/${images.image1}`} alt={`${baseImgUrl}/${images.image1} `} className="w-full h-full object-cover" />
+          {/* <img src={`${images.image1}`} alt={`${images.image1}`} className="w-full h-full object-cover" /> */}
 
         </SwiperSlide>
         <SwiperSlide>
 
-          {/* <img src={`${baseImgUrl}/${images.image2}`} alt={`Banner `} className="w-full h-full object-cover" /> */}
-          <img src={`${images.image2}`} alt={`${images.image1}`} className="w-full h-full object-cover" />
+          <img src={`${baseImgUrl}/${images.image2}`} alt={`Banner `} className="w-full h-full object-cover" />
+          {/* <img src={`${images.image2}`} alt={`${images.image1}`} className="w-full h-full object-cover" /> */}
 
         </SwiperSlide>
         <SwiperSlide>
 
-          {/* <img src={`${baseImgUrl}/${images.image3}`} alt={`Banner `} className="w-full h-full object-cover" /> */}
-          <img src={`${images.image3}`} alt={`${images.image1}`} className="w-full h-full object-cover" />
+          <img src={`${baseImgUrl}/${images.image3}`} alt={`Banner `} className="w-full h-full object-cover" />
+          {/* <img src={`${images.image3}`} alt={`${images.image1}`} className="w-full h-full object-cover" /> */}
         </SwiperSlide>
       </Swiper>
     </div>
