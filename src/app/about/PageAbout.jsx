3806850fdaac1d3e2b3a2ff90200d7fd86006a8e
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2023-11-06 19:35:21 +0530

    added facebook embeding

--- src/app/about/PageAbout.jsx
+++ src/app/about/PageAbout.jsx
@@ -66,7 +66,9 @@ const PageAbout = ({ }) => {
 
         <SectionStatistic />
 
-        <div className="flex">
+        <div className="flex flex-wrap shadow-lg">
+          <iframe className="m-auto" name="f1b4a881d42795" width="632px" height="500px" data-testid="fb:page Facebook Social Plugin" title="fb:page Facebook Social Plugin" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" src="https://www.facebook.com/v18.0/plugins/page.php?adapt_container_width=true&amp;app_id=&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Dfcf7784493df0c%26domain%3Diframely.net%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fiframely.net%252Ff2d06cca3d6ae28%26relation%3Dparent.parent&amp;container_width=0&amp;height=500&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FDOONSILK&amp;locale=en_US&amp;sdk=joey&amp;show_facepile=false&amp;show_posts=true&amp;small_header=false&amp;width=632" />
+        
           <iframe class="m-auto w-[300px] md:w-[600px] h-[600px] instagram-media instagram-media-rendered" id="instagram-embed-1" src="https://www.instagram.com/doon.silk/embed/?cr=1&amp;v=12&amp;wp=540&amp;rd=https%3A%2F%2Fwww.instaembedcode.com&amp;rp=%2F#%7B%22ci%22%3A1%2C%22os%22%3A18995.20000000298%2C%22ls%22%3A8454.89999999106%2C%22le%22%3A8923.5%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" data-instgrm-payload-id="instagram-media-payload-1" scrolling="no" />
         </div>
 
